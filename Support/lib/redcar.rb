1645730dcf4e7c7a72422f5fba888bc83d7cec68
Daniel Lucraft <dan@fluentradical.com>
2009-03-15 09:37:17 +0000

    Added redcar html theme

--- Support/lib/redcar.rb
+++ Support/lib/redcar.rb
@@ -4,11 +4,12 @@ require 'open3'
 module Kernel
   def `(str)
     result, error = nil, nil
-    File.open("tmp.sh", "w") {|fout| fout.puts str}
-    Open3.popen3("bash tmp.sh") do |stdin, stdout, stderr| 
+    File.open("redcar_tmp.sh", "w") {|fout| fout.puts str}
+    Open3.popen3("bash redcar_tmp.sh") do |stdin, stdout, stderr| 
       result = stdout.read
       error = stderr.read 
     end
+    FileUtils.rm_f("redcar_tmp.sh")
     puts error
     result
   end
